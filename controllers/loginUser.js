7c1c433de59f5d55cd7aed1ef74bc9d08d2f9d5a
AlexisMundo <56802786+AlexisMundo@users.noreply.github.com>
2021-06-30 17:59:44 -0700

    Add files via upload

---
+++ controllers/loginUser.js
@@ -0,0 +1,36 @@
+/*jshint esversion: 6*/
+
+
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
+const User = require('../models/user');
+
+module.exports = (req, res) =>{
+    const {username, password} = req.body;
+
+    console.log(username);
+    console.log(password);
+    User.findOne({username: username}, (error, user) => {
+
+        if(user){
+            bcrypt.compare(password, user.password,(error, same) =>{
+
+                if(same){
+                    console.log('si entro a same');
+                    res.redirect('/');
+                }
+                else {
+                    console.log('no es el mismo password');
+                    res.redirect('/auth/login');
+                }
+            });
+        }
+
+        else{
+            console.log('No encuentra usuario');
+            res.redirect('/auth/login');
+        }
+
+    });
+
+};
