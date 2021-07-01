adcea03273f97dce6705f2256b0c1fa99dd8903f
AlexisMundo <56802786+AlexisMundo@users.noreply.github.com>
2021-06-30 18:29:23 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const mongoose  = require("mongoose");
 
 module.exports = {
   port: process.env.PORT || 3000,
-  db: process.env.MONGODB || 'mongodb+srv://alexismundo:leviatan2@cluster0.kmry7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  db: process.env.MONGODB || 'mongodb+srv://alexismundo:leviatan2@cluster0.kmry7.mongodb.net/Cluster0?retryWrites=true&w=majority',
   
   urlParser: {
     useNewUrlParser: true,
